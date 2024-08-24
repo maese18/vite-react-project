672dc9d1efd8c502ab1d1242ceef9f8e96a3a2dc
Marcel Schleinkofer <marcel.schleinkofer@innotix.com>
2024-08-24 15:18:38 +0200

    sideeffects in useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(10)
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((prevCounter) => prevCounter + 1);
+    }, 2000);
+    return () => clearInterval(interval);
+  }, []);
+  
   return (
     <>
       <div>
@@ -18,7 +24,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount( count - 1)}>
           count is {count}
         </button>
         <p>
