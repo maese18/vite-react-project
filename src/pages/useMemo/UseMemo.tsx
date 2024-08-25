1cf08e6e80608e13a3a62603c0d2dcf0841d0ad0
Marcel Schleinkofer <marcel.schleinkofer@innotix.com>
2024-08-25 13:27:15 +0200

    solution with useMemo hook

--- src/pages/useMemo/UseMemo.tsx
+++ src/pages/useMemo/UseMemo.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { initialItems } from "./utils";
 
 interface UseMemoDemoProps {
@@ -8,13 +8,16 @@ export default function UseMemoDemo({ initialCount = 0 }: UseMemoDemoProps) {
   const [count, setCount] = useState(initialCount);
   const [items] = useState(initialItems);
 
-  const selectedItem = items.find((item) => item.isSelected);
+  const selectedItem = useMemo(
+    () => items.find((item) => item.isSelected),
+    [items]
+  );
 
   return (
     <div className="tutorial">
       <h1>Count: {count}</h1>
       <h1>Selected Item: {selectedItem?.id}</h1>
-      <button onClick={()=>setCount(count+1)}>Increment</button>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
     </div>
-  )
+  );
 }
