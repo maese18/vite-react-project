dca19df8bd7cc0da095319f5f700d0ceef6ec45b
Marcel Schleinkofer <marcel.schleinkofer@innotix.com>
2024-08-25 13:23:38 +0200

    useMemo initial

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import UseMemoDemo from './pages/useMemo/UseMemo'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <div>
+      <UseMemoDemo initialCount={1}></UseMemoDemo>
+    </div>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
---
+++ src/pages/useMemo/UseMemo.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { initialItems } from "./utils";
+
+interface UseMemoDemoProps {
+  initialCount: number;
+}
+export default function UseMemoDemo({ initialCount = 0 }: UseMemoDemoProps) {
+  const [count, setCount] = useState(initialCount);
+  const [items] = useState(initialItems);
+
+  const selectedItem = items.find((item) => item.isSelected);
+
+  return (
+    <div className="tutorial">
+      <h1>Count: {count}</h1>
+      <h1>Selected Item: {selectedItem?.id}</h1>
+      <button onClick={()=>setCount(count+1)}>Increment</button>
+    </div>
+  )
+}
