0b385f7bcf4ab86f6bfb45bc7f284a5ac380989f
Marcel Schleinkofer <marcel.schleinkofer@innotix.com>
2024-08-26 22:04:38 +0200

    renamed folder

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import UseCallback from './pages/useContext/UseCallback'
+import UseCallback from './pages/useCallback/UseCallback'
 
 function App() {
   const [count, setCount] = useState(0)
